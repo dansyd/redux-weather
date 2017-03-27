1d9ef5592db06f7ca33be6762b3d6f4321c766ee
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-27 19:10:44 +1100

    imported axios and implemented an action creator with AJAX request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '1db9e14c148c226960ff0928afaedfaf';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},au`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER
+    payload: request
+  }
+}
