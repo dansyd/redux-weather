6bfee28ff69a4022a169cf1159c793baec62f47b
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-27 18:49:46 +1100

    Handle submit, installed redux-promise

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '1db9e14c148c226960ff0928afaedfaf';
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,23 +5,26 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
-
     //binding
-    this.onInputChange = this.onInputChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  onInputChange(event) {
-    this.setState({ term: event.target.value });
+  handleChange(evt) {
+    this.setState({ term: evt.target.value });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
   }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.handleSubmit} className="input-group">
         <input
           placeholder="Get a 5 days forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange} />
+          onChange={this.handleChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
