83d2dc15ad1af952c96126ebaab1aebfc6eb6b25
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-28 18:20:00 +1100

    Created reducer to handle action for weather API request response

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,8 +8,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},au`;
   const request = axios.get(url);
+  
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
     payload: request
   }
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
     this.state = { term: '' };
     //binding
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(evt) {
@@ -15,6 +19,10 @@ export default class SearchBar extends Component {
 
   handleSubmit(evt) {
     evt.preventDefault();
+
+    // go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -33,3 +41,9 @@ export default class SearchBar extends Component {
   }
 
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
